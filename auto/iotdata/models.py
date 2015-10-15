fc56e9710f3f889dbd50dd6795053d5c17a66e33
Ryan Gerlach <gerlachry@gmail.com>
2015-10-15 19:46:50 -0400

    models version 1 and initial migrate

--- auto/iotdata/models.py
+++ auto/iotdata/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
+class Locations(models.Model):
+    name = models.CharField(max_length=255);
+    active = models.BooleanField();
+    create_date = models.DateTimeField('date record was created');
+    modified_date = models.DateTimeField('date record was last modified');
+
+
+class Sensors(models.Model):
+    device_id = models.CharField(max_length=255);
+    location_id = models.ForeignKey(Locations);
+    name = models.CharField(max_length=255);
+    purchased_from = models.CharField(max_length=255);
+    manufacturer = models.CharField(max_length=255);
+    active = models.BooleanField();
+    create_date = models.DateTimeField('date record was created');
+    modified_date = models.DateTimeField('date record was last modified');
+
+
+class ReadingType(models.Model):
+    name = models.CharField(max_length=255);
+    active = models.BooleanField();
+    create_date = models.DateTimeField('date record was created');
+    modified_date = models.DateTimeField('date record was last modified');
+
+class Readings(models.Model):
+    sensor_id = models.ForeignKey(Sensors);
+    reading_type_id = models.ForeignKey(ReadingType);
+    value = models.TextField();
+    info = models.TextField();
+    create_date = models.DateTimeField();
 
