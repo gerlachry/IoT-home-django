61448fb3a33894316aee68f9c4d7f7c799932fd6
Ryan Gerlach <gerlachry@gmail.com>
2015-10-31 20:54:33 -0400

    WIP basic ES service for POST Readings data

--- auto/iotdata/views.py
+++ auto/iotdata/views.py
@@ -3,6 +3,14 @@ from rest_framework.views import APIView
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from elasticsearch import Elasticsearch
+from auto.localsettings import ES_HOST, ES_PORT
+import json
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+es = Elasticsearch(host=ES_HOST, port=ES_PORT)
 
 class Readings(APIView):
     """API for handling sensor reading feeds which in turn index to elasticsearch
@@ -10,11 +18,17 @@ class Readings(APIView):
     """
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated, )
+    def __init__(self):
+        self.log = logging.getLogger('Readings')
 
     def post(self, request):
-        print request.data
-        print 'return posted data for testing'
-        return Response(request.data)
+        json_data = json.loads(request.body)
+        #index will be device name
+        idx = json_data['device_name']
+        self.log.info('writing to index %s data %s' % (idx, json_data))
+        resp = es.index(index=idx, doc_type='sensor', id=None,  body=json_data)
+        self.log.info('Indexing resp %s' % resp) 
+        return Response(resp)
 
     def get(self, request):
         results = 'all is ok'
